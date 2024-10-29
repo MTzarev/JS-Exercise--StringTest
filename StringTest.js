8cf79ed5221805b73c9edfb5b33314fd9e48c795
Miroslav Tsarev <mtzarev@gmail.com>
2024-10-29 22:37:13 +0200

    Update StringTest.js

--- StringTest.js
+++ StringTest.js
@@ -1,11 +1,11 @@
 function stringGame (input){
-let stringToChange = input.shift()
+let stringToChange = input.shift();
 for(let i=0; i<input.length; i++){
     let command = input[i].split(` `);
     if (command[0]===`Change`){
-        let toReplace= command[1]
-        let replacer = command[2]
-        stringToChange=stringToChange.replaceAll(toReplace, replacer)
+        let toReplace= command[1];
+        let replacer = command[2];
+        stringToChange=stringToChange.replaceAll(toReplace, replacer);
         console.log(stringToChange);
     }else if (command[0]===`Includes`){
         if(stringToChange.includes(command[1])){
@@ -14,12 +14,12 @@ for(let i=0; i<input.length; i++){
             console.log(`False`);
         }
     }else if(command[0]===`End`){
-        let length = command[1].length
-        let symbolsToFind = command[0].substring(command[0].length-length)
+        let length = command[1].length;
+        let symbolsToFind = command[0].substring(command[0].length-length);
         if(symbolsToFind===command[1]){
-            console.log(`True`)
+            console.log(`True`);
         }else if(symbolsToFind!==command[1]){
-            console.log(`False`)
+            console.log(`False`);
         }
     }else if (command[0]===`Uppercase`){
         stringToChange=stringToChange.toUpperCase()
@@ -30,9 +30,9 @@ for(let i=0; i<input.length; i++){
         console.log(idx);
     }else if(command[0]===`Cut`){
         let startIdx=Number(command[1]);
-        let endIdx=Number(command[2])
+        let endIdx=Number(command[2]);
         //let toPrint = stringToChange.substring(startIdx,(startIdx+endIdx));
-        let toPrint = stringToChange.substring(startIdx, (startIdx+endIdx))
+        let toPrint = stringToChange.substring(startIdx, (startIdx+endIdx));
         console.log(toPrint);
     }else if(command[0]===`Done`){
         break;
